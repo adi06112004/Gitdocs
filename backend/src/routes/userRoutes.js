64f6a25412105dc5c554094501d7991990d6fc1c
Abhishek Sharma <as6911904@gmail.com>
2026-04-12 21:34:38 +0530

    fixed Route Issues

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,22 +1,29 @@
 import express from "express";
 import {
+
   getUsers,
   getUserById,
   getMe,
   updateMe,
   changePassword,
   updatePreferences,
-} from "../controllers/userController.js";
+
+} 
+from "../controllers/userController.js";
 import { protect } from "../middleware/auth.js";
 
+
 const router = express.Router();
+
 
 router.get("/me", protect, getMe);
 router.patch("/me", protect, updateMe);
 router.patch("/me/password", protect, changePassword);
 router.patch("/me/preferences", protect, updatePreferences);
 
+
 router.get("/", protect, getUsers);
 router.get("/:id", protect, getUserById);
+
 
 export default router;
